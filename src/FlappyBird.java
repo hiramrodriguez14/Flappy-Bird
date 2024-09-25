7bd4702a1f2d911072f006ce0bd43389e2286234
hiramrodriguez14 <hiram.rodriguez14@upr.edu>
2024-09-25 01:32:15 -0400

    Initial Commit- Game with Background

---
+++ src/FlappyBird.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random; 
+import javax.swing.*;
+
+public class FlappyBird extends JPanel {
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    //Images
+    Image backgroundImage;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+
+        FlappyBird(){
+            setPreferredSize(new Dimension(boardWidth, boardHeight));
+            //setBackground(Color.blue);
+
+            //load images
+            backgroundImage = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+            birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+            topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+            bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        }
+
+        public void paintComponent(Graphics g){
+            super.paintComponent(g);
+            draw(g);
+        }  
+        
+        public void draw(Graphics g){
+            //background
+            g.drawImage(backgroundImage,0,0,boardWidth, boardHeight, null);
+        }
+}
